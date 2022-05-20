ac8c66e24df094d4f57366d4185c72a516fd4dd0
michael <michael.schaffler@ciit.at>
2022-05-20 14:04:26 +0200

    ..

--- Codebeispiele/quarkus/c-fault-tolerance-server/src/main/java/com/jambit/SecuredResource.java
+++ Codebeispiele/quarkus/c-fault-tolerance-server/src/main/java/com/jambit/SecuredResource.java
@@ -1,16 +1,27 @@
 package com.jambit;
 
 import io.quarkus.security.Authenticated;
+import io.smallrye.jwt.auth.principal.DefaultJWTCallerPrincipal;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.jwt.Claims;
 
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+import java.security.Principal;
+import java.util.Optional;
 
 @Path("/secured")
+@Slf4j
 public class SecuredResource {
+    @Inject
+    SecurityContext ctx;
+
     @GET
     @Path("/public")
     @Produces(MediaType.APPLICATION_JSON)
@@ -23,6 +34,14 @@ public class SecuredResource {
     @Produces(MediaType.APPLICATION_JSON)
     @Authenticated
     public String authenticatedAccess(){
+        DefaultJWTCallerPrincipal userPrincipal = (DefaultJWTCallerPrincipal)ctx.getUserPrincipal();
+        String birthdate = userPrincipal.claim(Claims.birthdate).orElse("").toString();
+
+        String name = userPrincipal.getName();
+        log.info("Username: " + name);
+        log.info("Birthdate: " + birthdate);
+        boolean isUser = ctx.isUserInRole("User");
+        log.info("has role User? " + isUser);
         return "you are authenticated";
     }
 
